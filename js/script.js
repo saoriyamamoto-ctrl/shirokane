3d4b4d68b3673d9768206b98fa43152280e47530
saoriyamamoto-ctrl <77010129+saoriyamamoto-ctrl@users.noreply.github.com>
2021-01-06 14:23:56 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ $(function() {
 
       if ($(this).hasClass('active')) {
           $('.globalMenuSp').addClass('active');
+        $("nav.globalMenuSp.active").fadeIn();
       } else {
           $('.globalMenuSp').removeClass('active');
+         $("nav.globalMenuSp.active").fadeOut();
       }
   });
 });
